21a6d945465730da63dad28097b3648dd3ae3166
Michael Warner <michaelwarner@Michaels-MacBook-Pro-2.local>
2023-08-25 10:14:39 -0400

    add open-graph url meta-tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
 /** Add your relevant code here for the issue to reproduce */
+
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  openGraph: {
+    url: '/'
+  }
+}
+
 export default function Home() {
   return null
 }
